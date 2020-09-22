e4266984b93f834cd23723a455fd6b8f7c9c22e5
Bruce A. Mah <bmah@kitchenlab.org>
2020-09-22 08:15:30 -0700

    enh: Parameterize UPS name and make it a tag. Improve string parsing.

--- getUpsData.py
+++ getUpsData.py
@@ -2,7 +2,12 @@
 
 import subprocess
 
-cmd="upsc ups"
+ups_name="study"
+upsc_command="upsc"
+measurement_name="ups"
+
+cmd=upsc_command + " " + ups_name
+
 output=""
 string_measurements=["battery.mfr.date", "battery.type", "device.mfr", "device.model","device.serial","device.type",
 			"driver.name", "driver.parameter.port", "driver.parameter.synchronous", "driver.version", "driver.version.data",
@@ -15,13 +20,29 @@ for line in p.stdout.readlines(): #read and store result in log file
     key = line[:line.find(":")]
     value = line[line.find(":")+2:]
 
+    # Determine whether a given value should be treated as a string.
+    # If the key is included in string_measurements, we treat as a
+    # string regardless.  Otherwise, see if a conversion to an
+    # integer or a float can succeed...if not we assume a string.
+    stringify = False
     if key in string_measurements:
+        stringify = True
+    else
+        try:
+            val = int(value)
+        except ValueError:
+            try:
+                val = float(value)
+            except ValueError:
+                stringify = True
+
+    if stringify:
         value = '"' + value + '"'
     measurement = key + "=" + value
     if output != "":
         measurement = "," + measurement
     output += measurement
 
-output = "ups " + output.rstrip()
+output = measurement_name + ",ups=" + ups_name + " " + output.rstrip()
 print(output, end='')
 
